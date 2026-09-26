d151806b7eeb64dc5a20a87a426a773a9c0ba1a7
temamhussen619-sketch <temamhussen619@gmail.com>
2026-09-26 15:54:22 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,22 @@
 import 'package:flutter/material.dart';
+import 'video_feed_screen.dart';
 
 void main() {
-  runApp(const AlphaApp());
+  runApp(const MyApp());
 }
 
-class AlphaApp extends StatelessWidget {
-  const AlphaApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Alpha Motivation',
       debugShowCheckedModeBanner: false,
+      title: 'Alpha Motivation',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-        useMaterial3: true,
+        brightness: Brightness.dark,
       ),
-      home: const HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Alpha Motivation'),
-        centerTitle: true,
-        backgroundColor: Colors.deepOrange,
-        foregroundColor: Colors.white,
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Icon(
-                Icons.auto_awesome,
-                size: 80,
-                color: Colors.deepOrange,
-              ),
-              SizedBox(height: 20),
-              Text(
-                'እንኳን ወደ Alpha Motivation መተግበሪያ በደህና መጡ!',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 10),
-              Text(
-                'የስኬትና የመነቃቃት መንገድዎ እዚህ ይጀምራል።',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.grey,
-                ),
-                textAlign: TextAlign.center,
-              ),
-            ],
-          ),
-        ),
-      ),
+      home: const VideoFeedScreen(),
     );
   }
 }
